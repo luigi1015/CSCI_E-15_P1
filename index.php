874fa1d1f1c3103392b9a5325d50617f6a40e9c1
Jeff <github@314ghz.com>
2016-02-11 14:26:34 -0600

    Fixed a problem with index.php not showing up in Git.

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="p1.css">
+		<title>CSCI E-15 Project 1 - Jeff Crone</title>
+	</head>
+	<body>
+		<center>
+			<h1>CSCI E-15 Project 1</h1>
+			<h2>Jeff Crone</h2>
+			<img src="images/reading.jpg" alt="Jeff Crone" width="460" height="540">
+		</center>
+		<table>
+			<tr>
+				<td>
+					<h3>Project 2</h3>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+				<td>
+					<h3>Project 3</h3>
+					<br>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+				<td>
+					<h3>Project 4</h3>
+					<br>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
