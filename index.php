106a79ff1db0c2139771935560bc209544172528
Jeff <github@314ghz.com>
2016-02-11 00:24:09 -0600

    Took out the underline tag in index.php to do a little more testing of my process.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 		<title>This is a test</title>
 	</head>
 	<body>
-		<h1><u>If you can see this, I got my process to work.</u></h1>
+		<h1>If you can see this, I got my process to work.</h1>
 	</body>
 </html>
