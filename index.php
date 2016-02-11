197f9857f0f424323d0575bd1717ff5880c79bc1
Jeff <github@314ghz.com>
2016-02-11 14:51:14 -0600

    Resubmitted index.php again.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="p1.css">
+		<title>CSCI E-15 Project 1 - Jeff Crone</title>
+	</head>
+	<body>
+		<h1>CSCI E-15 Project 1</h1>
+		<h2>Jeff Crone</h2>
+		<img src="images/reading.jpg" alt="Jeff Crone" width="460" height="540">
+		<br>
+		<table>
+			<tr>
+				<td>
+					<h3>Project 2</h3>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+				<td>
+					<h3>Project 3</h3>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+				<td>
+					<h3>Project 4</h3>
+					Live version: [link]
+					<br>
+					Github version: [link]
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
